0b8dfba14300f363623755cdbd3f329388edf82b
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-21 18:14:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,4 @@
 class CashRegister
-  attr_accessor :discount, 
-  
-  def initialize(discount = nil)
-    @total = 0 
-  end 
-  
-end
-
-
-class CashRegister
   
   attr_accessor :discount, :total, :items, :previous_total
   
