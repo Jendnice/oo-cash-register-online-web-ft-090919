d979b8619b10764df30ef4cca4af6c758e735be8
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-21 18:07:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,4 +18,15 @@ class CashRegister
   
   def apply_discount
     if discount != nil
-      @total = (@total * (1.to_f - (discount.to_f / 100))).to
+      @total = (@total * (1.to_f - (discount.to_f / 100))).to_i
+      "After the discount, the total comes to $#{total}."
+    else
+      "There is no discount to apply."
+    end
+  end 
+  
+  def void_last_transaction
+    @total = @previous_total
+  end
+end 
+
