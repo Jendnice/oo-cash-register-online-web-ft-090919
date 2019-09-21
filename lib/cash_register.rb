13ea35f5afbb72c6cdb731a4c4f1b412ff7c5198
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-21 18:06:35 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,21 @@
 class CashRegister
   
-  attr_accessor :discount, :total, :items, 
+  attr_accessor :discount, :total, :items, :previous_total
+  
+  def initialized(discount = nil)
+    @total = 0 
+    @discount = discount
+    @items = []
+  end 
+  
+  def add_item(title, price, quantity = 1)
+    @previous_total = @total
+    @total += (price * quantity)
+    quantity.times do 
+      @items << title 
+    end 
+  end 
+  
+  def apply_discount
+    if discount != nil
+      @total = (@total * (1.to_f - (discount.to_f / 100))).to
